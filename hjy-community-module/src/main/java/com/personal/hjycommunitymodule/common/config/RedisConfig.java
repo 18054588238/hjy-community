ad582dc6bf6d509ec4eab8384182554fbbae71b7
liupanpan <liuppcn@163.com>
2025-12-08 19:30:24 +0800

    redis

---
+++ hjy-community-module/src/main/java/com/personal/hjycommunitymodule/common/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.personal.hjycommunitymodule.common.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @ClassName RedisConfig
+ * @Author liupanpan
+ * @Date 2025/12/8
+ * @Description
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    @SuppressWarnings(value = {"unchecked","rawtypes"})
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        FastJsonRedisSerializer serializer = new FastJsonRedisSerializer(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+        // 指定要序列化的域
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+
+        // 序列化redis中存储的value值
+        redisTemplate.setValueSerializer();
+
+
+        return redisTemplate;
+    }
+}
--- hjy-community-module/src/test/java/com/personal/hjycommunitymodule/HjyCommunityModuleApplicationTests.java
+++ hjy-community-module/src/test/java/com/personal/hjycommunitymodule/HjyCommunityModuleApplicationTests.java
@@ -1,5 +1,6 @@
 package com.personal.hjycommunitymodule;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -18,10 +19,13 @@ class HjyCommunityModuleApplicationTests {
 
     @Test
     void testJJWT() {
+        long l = System.currentTimeMillis() + 10000000l;
         JwtBuilder builder = Jwts.builder()
                 .setId("1128") // 设置jti (JWT ID) - payload
                 .setSubject("hejiayun_community") // sub (subject) 设置主题 - payload
                 .setIssuedAt(new Date()) // iat (issued at)设置颁发时间 - payload
+                .setExpiration(new Date(l)) // 设置过期时间
+                .claim("role", "admin") // 自定义载荷信息 ，以存储更多的信息
                 .signWith(SignatureAlgorithm.HS256,"mashibing");// 设置签名
         // 构建 JWT 并根据 JWT 紧凑序列化规则将其序列化为紧凑的、URL 安全的字符串,签名后的jwt成为jws
         String jws = builder.compact();
@@ -31,6 +35,11 @@ class HjyCommunityModuleApplicationTests {
 
     @Test
     void parserJWT() {
-
+        String jws = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMTI4Iiwic3ViIjoiaGVqaWF5dW5fY29tbXVuaXR5IiwiaWF0IjoxNzY1MTg5NTgyLCJleHAiOjE3NjUxOTk1ODIsInJvbGUiOiJhZG1pbiJ9.Or_G2BBxOgClaf4fs3W-lzi18M8wTJ_d0KKJoQJy_WY";
+        Claims claims = Jwts.parser()
+                .setSigningKey("mashibing") // 密钥
+                .parseClaimsJws(jws)
+                .getBody();
+        System.out.println(claims);// 输出payload中的内容
     }
 }

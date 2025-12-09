abe5abdeee9c267171d6c08ee6bd3d3cb689a0c3
盼盼 刘 <liuppcn@163.com>
2025-12-09 20:48:49 +0800

    spring security

--- hjy-community-module/src/main/java/com/personal/hjycommunitymodule/common/config/SpringSecurityConfig.java
+++ hjy-community-module/src/main/java/com/personal/hjycommunitymodule/common/config/SpringSecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,11 +24,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    // 配置http请求的安全处理
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // 关闭csrf
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)// 不会创建会话，每个请求都将被视为独立的请求
+                .and()
+                .authorizeRequests() // 定义请求授权规则
+                .antMatchers("/hejiayun/user/login").anonymous()//登录接口，允许匿名访问
+                .anyRequest().authenticated();
+        super.configure(http);
     }
 
+    // 注入AuthenticationManager，供外部使用
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
--- hjy-community-module/src/main/java/com/personal/hjycommunitymodule/community/service/impl/LoginServiceImpl.java
+++ hjy-community-module/src/main/java/com/personal/hjycommunitymodule/community/service/impl/LoginServiceImpl.java
@@ -1,9 +1,18 @@
 package com.personal.hjycommunitymodule.community.service.impl;
 
 import com.personal.hjycommunitymodule.common.core.domain.BaseResponse;
+import com.personal.hjycommunitymodule.common.utils.JWTUtils;
+import com.personal.hjycommunitymodule.common.utils.RedisCache;
+import com.personal.hjycommunitymodule.community.domain.LoginUser;
 import com.personal.hjycommunitymodule.community.domain.SysUser;
 import com.personal.hjycommunitymodule.community.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 /**
  * @ClassName LoginServiceImpl
@@ -13,10 +22,29 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LoginServiceImpl implements LoginService {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    RedisCache redisCache;
+
     @Override
     public BaseResponse login(SysUser user) {
-        // 调用AuthenticationManager中DaoAuthenticationProvider的authenticate方法进行认证
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword());
+        // 调用(AuthenticationManager 实现类ProviderManager 委托给)DaoAuthenticationProvider的authenticate方法进行认证
+        Authentication authenticate = authenticationManager.authenticate(authentication);
+        if (authenticate == null) {
+            throw new RuntimeException("登录失败");// 认证失败
+        }
+        // 认证成功，使用userId生成token
+        LoginUser principal = (LoginUser) authenticate.getPrincipal();
+        Long userId = principal.getSysUser().getUserId();
+        String jwt = JWTUtils.createJWT(String.valueOf(userId));
 
-        return null;
+        // 将用户信息存储到redis缓存中
+        redisCache.setCacheObject("login:"+userId,principal);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("token",jwt);
+        return BaseResponse.success(map);
     }
 }
